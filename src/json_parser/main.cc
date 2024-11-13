00fd2558b0833d2fbf0bdda65d1d5966b04915c8
pkj <pkjifinity@outlook.com>
2024-11-13 23:30:37 +0800

    feat:dotfiles update

--- src/json_parser/main.cc
+++ src/json_parser/main.cc
@@ -1,13 +1,15 @@
+#include <dbg.h>
 #include <iostream>
+#include <json/json.h>
 #include <spdlog/common.h>
 #include <spdlog/spdlog.h>
-#include <json/json.h>
-#include <dbg.h>
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
   spdlog::set_level(spdlog::level::debug);
   spdlog::debug("hello world");
   std::cout << "hello world" << std::endl;
   dbg("hello world");
+  spdlog::debug("hello world");
+  spdlog::set_level(spdlog::level::info);
   return 0;
 }
