34b5c375e3f9fc349a13c00d0f2be106da0c0f11
pkj <pkjifinity@outlook.com>
2024-11-13 23:32:02 +0800

    -

--- src/json_parser/main.cc
+++ src/json_parser/main.cc
@@ -1,15 +1,24 @@
 #include <dbg.h>
-#include <iostream>
 #include <json/json.h>
 #include <spdlog/common.h>
 #include <spdlog/spdlog.h>
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
-{
-  spdlog::set_level(spdlog::level::debug);
-  spdlog::debug("hello world");
-  std::cout << "hello world" << std::endl;
-  dbg("hello world");
-  spdlog::debug("hello world");
-  spdlog::set_level(spdlog::level::info);
-  return 0;
+
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+struct Person {
+    std::string name;
+    int age;
+    std::string address;
+};
+int main( [[maybe_unused]] int argc, [[maybe_unused]] char* argv[] ) {
+    spdlog::set_level( spdlog::level::debug );
+    spdlog::debug( "hello world" );
+    spdlog::info( "hello world" );
+    std::cout << "hello world" << std::endl;
+    std::map< int, int > mymap;
+    dbg( "hello world" );
+    std::queue< Person > que;
+    return 0;
 }
